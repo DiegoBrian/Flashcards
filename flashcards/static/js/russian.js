261c536e7a1120a3d9fc0ab857eaff076b25a056
DiegoBrian <diegobleite@hotmail.com>
2018-11-01 14:56:34 -0300

    Audio in card's front side

--- flashcards/static/js/russian.js
+++ flashcards/static/js/russian.js
@@ -1,13 +1,17 @@
+$(document).ready( function() {
+
 $(".card").flip({
   axis: 'y',
   trigger: 'manual'
 });
 
-$("img").click(function(){
+$(".audio").click(function(){
    //$('.sound')[0].play()
    $(this).parent().siblings('.sound')[0].play();
 });
 
 $(".flip-btn").click(function(){
   $(this).closest(".card").flip(true);
+});
+
 });
