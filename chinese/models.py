5d20ac81b359150b90f1780436fb11c339bf49f3
DiegoBrian <diegobleite@hotmail.com>
2018-11-23 11:29:38 -0200

    Falas em chinês adicionadas ao BD

--- chinese/models.py
+++ chinese/models.py
@@ -2,10 +2,13 @@ from django.db import models
 from django.conf import settings
 
 class Sentence(models.Model):
-	number = models.IntegerField('Number', default=0)
+	number = models.IntegerField('Number', default=0, blank= True, null=True)
 	sentence = models.CharField('Sentence', max_length=3000)
-	pinyin = models.CharField('Pinyin', max_length=3000)
-	equivalence = models.CharField('Equivalence in English', max_length=3000)
+	pinyin = models.CharField('Pinyin', max_length=3000, blank= True, null=True)
+	equivalence = models.CharField('Equivalence in English', max_length=3000, blank= True, null=True)
+
+	def __str__(self):
+		return self.sentence
 	
 
 class User_Sentence(models.Model):
